84f3acd14fd460bc7dcd78fb682286b2a511958a
shepelev-official <shepelev.official@gmail.com>
2020-07-13 20:38:07 +0300

    form added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,45 +1,63 @@
 import React from 'react';
 
+
 const Form = () => {
 
   const props = {
-    type: "GridComponent",
-    metadata: {
-      components: [{
-        type: "RichTextComponent",
-        col: "md-6",
-        metadata: {
-          title: "О нас",
-          text: "<p>Структурный голод столь же важен для жизни, как и агрессия иллюстрирует объект, что лишний раз подтверждает правоту З.Фрейда. Субъект, как бы это ни казалось парадоксальным, непосредственно иллюстрирует архетип, Гоббс одним из первых осветил эту проблему с позиций психологии. Сознание выбирает конфликтный ассоцианизм. Реакция текстологически интегрирует филосовский аутизм.</p><p>Закон, согласно традиционным представлениям, начинает импульс. Проекция отталкивает тест одинаково по всем направлениям. Структурный голод столь же важен для жизни, как и коллективное бессознательное текстологически выбирает интеллект. Реакция отчуждает конформизм.</p>"
-        }
-      }, {
-        type: "RichTextComponent",
-        col: "md-6",
-        metadata: {
-          title: "Важно знать",
-          text: "<p>Абсолютная погрешность, в первом приближении, проецирует абстрактный интеграл по бесконечной области, что неудивительно. До недавнего времени считалось, что предел последовательности позиционирует анормальный криволинейный интеграл. Используя таблицу интегралов элементарных функций, получим: скалярное поле оправдывает интеграл по поверхности, дальнейшие выкладки оставим студентам в качестве несложной домашней работы. Подынтегральное выражение, конечно, накладывает абстрактный интеграл Пуассона. Уравнение в частных производных осмысленно поддерживает действительный определитель системы линейных уравнений.</p>"
-        }
-      }]
+    title: "Свяжитесь с нами",
+    fields: [
+      {"name": "name", "group": "main", "type": "text", "label": "Имя", "required": true},
+      {"name": "phone", "group": "main", "type": "phone", "label": "Телефон", "required": true},
+      {"name": "email", "group": "main", "type": "email", "label": "E-mail", "required": true},
+      {"name": "appointment_date", "group": "main", "type": "date", "label": "Дата записи", "required": true},
+      {"name": "name", "group": "additional", "type": "textarea", "label": "Комментарий", "required": false},
+      {
+        "name": "agreement",
+        "type": "checkbox",
+        "label": "<span>Я даю согласие на обработку персональных данных согласно <a href='#'>политике конфиденциальности</a>.</span>"
+      }
+    ],
+    field_groups: {
+      "main": "col-md-8",
+      "additional": "col-md-4"
+    },
+    submit_button: {
+      "text": "Отправить заявку"
     }
   }
 
 
   return (
-    <div className="container">
-      <div className="row">
-        {props.metadata.components.map((item, index) => {
+    <form>
+      <h1>{props.title}</h1>
+      <div className="form-row">
+        {props.fields.map((item, index) => {
+
+
+          const identifierGroup = (group) => {
+            if (group === "main") {
+              return "col-md-8"
+            }
+            if (group === "additional") {
+              return "col-md-4"
+            }
+          }
+
+
           return (
-            <div className={`col-${item.col}`} key={index}>
-              <h1>
-                {item.metadata.title}
-              </h1>
-              <div dangerouslySetInnerHTML={{__html: item.metadata.text}}/>
+            <div className={identifierGroup(item.group)} key={index}>
+              <label htmlFor={`${item.label}`}>
+                <div dangerouslySetInnerHTML={{__html: item.label}}/>
+                <input name={item.name} type={item.type} required={item.required} className="form-control"/>
+              </label>
+
             </div>
           )
-        })}
+        })
+        }
       </div>
-    </div>
-  );
+    </form>
+  )
 }
 
 export default Form;
